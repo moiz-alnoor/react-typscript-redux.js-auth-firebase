160c3ca9a83c11b94e65fe32518c8b3da3aced60
Moiz <58396876+moiz-alnoor@users.noreply.github.com>
2022-02-02 11:19:49 +0400

    remove firebase config

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -4,13 +4,7 @@ import 'firebase/compat/firestore'
 
 // connect with firebase for auth
 const firebaseConfig:object = {
-  apiKey: "AIzaSyAWJPh4DAKBF10I4DcDD8Yk844nbh6dXEw",
-  authDomain: "playtomic-d9d86.firebaseapp.com",
-  projectId: "playtomic-d9d86",
-  storageBucket: "playtomic-d9d86.appspot.com",
-  messagingSenderId: "66313153152",
-  appId: "1:66313153152:web:93c73178f0797fc20e1465",
-  measurementId: "G-0CK60VBHPJ"
+
 };
 
   // Initialize Firebase
